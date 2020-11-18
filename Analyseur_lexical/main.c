89657c7c17266ac951e2a1d1e219b575b374fd47
unknown <abderrahimdeaabane@gmail.com>
2020-11-18 03:39:50 +0100

    Analyseur lexical terminé

--- Analyseur_lexical/main.c
+++ Analyseur_lexical/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 typedef enum{
     PROGRAM_TOKEN,CONST_TOKEN,VAR_TOKEN,BEGIN_TOKEN,END_TOKEN,IF_TOKEN,THEN_TOKEN,WHILE_TOKEN,DO_TOKEN,READ_TOKEN,WRITE_TOKEN,
     PV_TOKEN,PT_TOKEN,PLUS_TOKEN,MOINS_TOKEN,MULT_TOKEN,DIV_TOKEN,VIR_TOKEN,EQ_TOKEN,AFF_TOKEN,INF_TOKEN,INFEG_TOKEN,SUP_TOKEN,SUPEG_TOKEN,
@@ -11,8 +12,19 @@ typedef struct {
     char NOM[20];
 }OUTPUT;
 
+char REVERSE_ENUM [50][20]={"PROGRAM_TOKEN","CONST_TOKEN","VAR_TOKEN","BEGIN_TOKEN","END_TOKEN","IF_TOKEN","THEN_TOKEN","WHILE_TOKEN","DO_TOKEN","READ_TOKEN","WRITE_TOKEN",
+"PV_TOKEN","PT_TOKEN","PLUS_TOKEN","MOINS_TOKEN","MULT_TOKEN","DIV_TOKEN","VIR_TOKEN","EQ_TOKEN","AFF_TOKEN","INF_TOKEN","INFEG_TOKEN","SUP_TOKEN","SUPEG_TOKEN","DIFF_TOKEN",
+"PO_TOKEN","PF_TOKEN","FIN_TOKEN","ID_TOKEN","NUM_TOKEN","ERREUR_TOKEN"};
+
 char CHAR_COUR;
 FILE *f;
+
+char *toLowerCase(char *s){
+    for(int i = 0; s[i]; i++){
+    s[i] = tolower(s[i]);
+}
+return s;
+}
 
 void LireChar(){
     CHAR_COUR =fgetc(f);
@@ -33,6 +45,7 @@ OUTPUT* LireMot(){
     }
     fseek(f,-1,SEEK_CUR);
     output->NOM[i] = '\0';
+    strcpy(output->NOM,toLowerCase(output->NOM));
     if(strcmp(output->NOM,"program")==0){
         output->Code=PROGRAM_TOKEN;
         return output;
@@ -88,18 +101,126 @@ OUTPUT* LireNum(){
     output->Code = NUM_TOKEN;
     return output;
 }
+OUTPUT* Lire_Car(){
+    int i=0;
+    OUTPUT *output = malloc(sizeof(OUTPUT));
+    output->NOM[i] = CHAR_COUR;
+    i++;
+    switch (CHAR_COUR)
+    {
+    case ';':
+        output->Code = PV_TOKEN;
+        break;
+        
+    case '.':
+        output->Code = PT_TOKEN;
+        break;
+        
+    case '+':
+        output->Code = PLUS_TOKEN;
+        break;
+        
+    case '-':
+        output->Code = MOINS_TOKEN;
+        break;
+        
+    case '*':
+        output->Code = MULT_TOKEN;
+        break;
+        
+    case '/':
+        output->Code = DIV_TOKEN;
+        break;
+        
+    case ',':
+        output->Code = VIR_TOKEN;
+        break;
+        
+    case ':':
+        LireChar();
+        if(CHAR_COUR=='='){
+            output->Code = AFF_TOKEN;
+            output->NOM[i]=CHAR_COUR;
+            i++;
+        }else{
+            output->Code = ERREUR_TOKEN;
+            output->NOM[i]=CHAR_COUR;
+            i++;
+        }
+        break;
+        
+    case '<':
+        LireChar();
+        if(CHAR_COUR=='='){
+            output->Code = INFEG_TOKEN;
+            output->NOM[i]=CHAR_COUR;
+            i++;
+            
+        }else if(CHAR_COUR=='>'){
+            output->Code = DIFF_TOKEN;
+            output->NOM[i]=CHAR_COUR;
+            i++;
 
+        }else{
+            output->Code = INF_TOKEN;
 
+        }
+
+        break;
+        
+     case '>':
+        LireChar();
+        if(CHAR_COUR=='='){
+            output->Code = SUPEG_TOKEN;
+            output->NOM[i]=CHAR_COUR;
+            i++;
+            
+        }else{
+            output->Code = INF_TOKEN;
+        }
+        break;
+    
+    case '(':
+        output->Code = PO_TOKEN;
+        break;
+        
+    case ')':
+        output->Code = PF_TOKEN;
+        break; 
+    case '=':
+        output->Code = EQ_TOKEN;
+        break; 
+    case EOF:
+        output->Code = FIN_TOKEN;
+        break;    
+    default:
+        output->Code = ERREUR_TOKEN;
+        break;
+    }
+    output->NOM[i] = '\0';
+    return output;
+}
+
+//Passer les separateurs et les commentaires
 void passer(){
-    if(CHAR_COUR==' '||CHAR_COUR=='\t'||CHAR_COUR=='\n'){
+    while(CHAR_COUR==' '||CHAR_COUR=='\t'||CHAR_COUR=='\n'){//Passer les separateurs
+            LireChar();
+        while(CHAR_COUR=='{'){//Passer les commentaires
+            LireChar();
+            if(CHAR_COUR=='*'){
+                while (CHAR_COUR!='}'&&CHAR_COUR!=EOF)
+                {
+
+                    while (CHAR_COUR!='*'&&CHAR_COUR!=EOF) LireChar();
+                    LireChar();
+                }
+            }else{
+                fseek(f,-1,SEEK_CUR);
+                break;
+            }
             LireChar();
         }
-        if(CHAR_COUR=='{'){
-            while (CHAR_COUR!='}'&&CHAR_COUR!=EOF)
-            {
-                LireChar();
-            }
-        }
+    }
         
 }
 
@@ -112,17 +233,19 @@ void analyseurSyntaxique(){
         //MOT
         if((CHAR_COUR>='A' && CHAR_COUR<='Z')||((CHAR_COUR>='a' && CHAR_COUR<='z'))){
             OUTPUT *Mot = LireMot();
-            //printf("%d\n",Mot->Code);
+            printf("%s\n",REVERSE_ENUM[Mot->Code]);
             free(Mot);
         }
         else{
             //Numero
             if(CHAR_COUR>='0' && CHAR_COUR<='9'){
                 OUTPUT *Num = LireNum();
-                printf("%s\n",Num->NOM);
+                printf("%s\n",REVERSE_ENUM[Num->Code] );
                 free(Num);
             }else{
-                Lire_Car();
+                OUTPUT * car = Lire_Car();
+                printf("%s\n",REVERSE_ENUM[car->Code]);
+                free(car);
             }
         }
 
